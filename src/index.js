d7f12a8d4ce8751fd331a123ee848082933e44f4
Jonas Deuchler <dejo1017@hs-karlsruhe.de>
2020-02-01 13:54:34 +0100

    Fixed CORS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // /Users/JD/mongodb/bin/mongod.exe --dbpath=/Users/JD/mongodb-data 
 
 const express = require("express")
-
 require("./db/mongoose")
+const cors = require("cors")
 const userRouter = require("./routes/userRouter.js")
 const logRouter = require("./routes/logRouter.js")
 
@@ -11,7 +11,7 @@ const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
-
+app.use(cors())
 app.use(logRouter)
 app.use(userRouter)
 
